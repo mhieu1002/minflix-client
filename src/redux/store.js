74fe1abf5e06a615c5e7d754177aed8b108fb0a5
Minh Hiếu <75494633+mhieu1002@users.noreply.github.com>
2023-03-03 15:03:06 +0700

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import appStateSlice from "./features/appStateSlice.js";
+import authModalSlice from "./features/authModalSlice.js";
+import globalLoadingSlice from "./features/globalLoadingSlice.js";
+import themeModeSlice from "./features/themeModeSlice.js";
+import userSlice from "./features/userSlice.js";
+
+const store = configureStore({
+  reducer: {
+    user: userSlice,
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
+    appState: appStateSlice
+  },
+});
+
+export default store;
